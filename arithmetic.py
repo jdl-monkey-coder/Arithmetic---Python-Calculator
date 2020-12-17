223112a84583e3cf802d83d1b9d4d64e36d980f9
JDLEE <josephdlee08@gmail.com>
2020-12-16 20:33:55 -0500

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,27 @@
+def multiply(a, b):
+    print(a*b)
+        
+def divide(c, d):
+    print(c/d)
+        
+def add(e, f):
+    print(e+f)
+        
+def subtract(g, h):
+    print(g-h)
+        
+def square(i):
+    print(i**2)
+        
+def cube(j):
+    print(j**3)
+        
+def sqrt(k):
+    print(k**(1/2))
+        
+def cbrt(l):
+    print(l**(1/3))
+        
+def modulo(m, n):
+    print(m % n)
+        
