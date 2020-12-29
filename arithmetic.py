842ed26b52c7e5f65544fd930f57fc9645407d2b
JDLEE <josephdlee08@gmail.com>
2020-12-29 11:36:20 -0500

    Added `try` and `except`

--- arithmetic.py
+++ arithmetic.py
@@ -1,32 +1,41 @@
-def multiply(a, b):
-    print(a*b)
+try:
+    def multiply(a, b):
+        print(a*b)
         
-def divide(c, d):
-    print(c/d)
+    def divide(c, d):
+        print(c/d)
         
-def add(e, f):
-    print(e+f)
+    def add(e, f):
+        print(e+f)
         
-def subtract(g, h):
-    print(g-h)
+    def subtract(g, h):
+        print(g-h)
         
-def square(i):
-    print(i**2)
+    def square(i):
+        print(i**2)
         
-def cube(j):
-    print(j**3)
+    def cube(j):
+        print(j**3)
         
-def sqrt(k):
-    print(k**(1/2))
+    def sqrt(k):
+        print(k**(1/2))
         
-def cbrt(l):
-    print(l**(1/3))
+    def cbrt(l):
+        print(l**(1/3))
         
-def modulo(m, n):
-    print(m % n)
-    
-def percentage(o, p):
-	print(o*(p/100))
+    def modulo(m, n):
+        print(m % n)
 
-def exp(q, r):
-    print(q**(r))
+    def percentage(o, p):
+        print(o*(p/100))
+
+    def exp(q, r):
+        print(q**(r))
+
+except ZeroDivisionError:
+    print('Undefined')
+
+except ValueError:
+    print('Invalid Input')
+except TypeError:
+    print('Invalid Input')
